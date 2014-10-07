ce21b59e79b0e704648bd4d03dcc5e10771979ef
1ztree <de.johnstn@gmail.com>
2014-10-06 22:15:36 -0400

    Update Initiative.py

--- Initiative.py
+++ Initiative.py
@@ -8,13 +8,15 @@ class Initiative:
 		self.initiative_order = []
 	
 	# Get the initiative order and return the array detailing the order.
+	# getOrder : -> List
 	def getOrder(self):
 		return self.initiative_order
 	
 	# Parses a regex to add new characters to the initiative list.
-	def addInitiative(self, regex):
+	# addInitiative : String -> Void
+	def addInitiative(self, expr):
 		group_size = 0
-		m = re.match("#Roll ([1-9][0-9]?)?\s?(.*)?\s?([1-9][0-9]?)D([1-9][0-9]?)\s?\+?\s?([1-9][0-9]?)?", regex)
+		m = re.match("#Roll ([1-9][0-9]?)?\s?(.*)?\s?([1-9][0-9]?)D([1-9][0-9]?)\s?\+?\s?([1-9][0-9]?)?", expr)
 		group_size = int(m.group(1))
 		name = m.group(2)
 		num_dice = m.group(3)
@@ -63,7 +65,9 @@ class Initiative:
 			return total_roll
 		else:
 			return total_roll + mod
-			
+	
+	#Updates the initiative list when a new round starts and sorts the list even if a new round has not begun.
+	# updateInitiative : Boolean -> Void
 	def updateInitiative(self, new_round):
 		if new_round:
 			for char in self.initiative_order:
@@ -72,12 +76,14 @@ class Initiative:
 					self.initiative_order.remove(char)
 		self.initiative_order = qsort(self.initiative_order)
 			
+	# Standard Quick Sort Algorithm.
+	# qsort : List -> List
 	def qsort(inlist):
     if inlist == []: 
-        return []
+		return []
     else:
-        pivot = inlist[0].getInit()
-        lesser = qsort([x for x in inlist[1:].getInit() if x < pivot])
-        greater = qsort([x for x in inlist[1:] if x >= pivot])
-        return lesser + [pivot] + greater
+		pivot = inlist[0].getInit()
+		lesser = qsort([x for x in inlist[1:].getInit() if x < pivot])
+		greater = qsort([x for x in inlist[1:] if x >= pivot])
+		return lesser + [pivot] + greater
 		
